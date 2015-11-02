607e97a39e982df4dd9a76e09fde248ed77668a6
Sean Morris <sean.smorris@gmail.com>
2015-11-02 10:05:00 -0800

    two dice

---
+++ source-code/lesson4/LeapDay/LeapDay.java
@@ -0,0 +1,24 @@
+/**
+ * Complete the setLeapDay method to properly detect leap days.
+ */
+
+ public class LeapDay {
+ 
+    private int monthNumber;
+    private int dateInMonth;
+    private boolean isLeapDay;
+ 
+    public LeapDay (){
+       
+    }
+
+    // write this
+    // Sets isLeapDay to true if monthNumber is equal to 2 and dateInMonth 
+    // equals 29. Otherwise isLeapYear is set to false.
+    public void setLeapDay() {
+            
+              
+       
+    }
+    
+}

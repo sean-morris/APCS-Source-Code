ded2127d317d77a0828aa4c856dabccd7ebc32f3
sean-morris <sean.smorris@gmail.com>
2017-10-05 11:11:32 -0700

    Update LeapYear.java

--- source-code/lesson4/LeapYear2/LeapYear.java
+++ source-code/lesson4/LeapYear2/LeapYear.java
@@ -4,17 +4,11 @@
 
 public class LeapYear{
 
-   private int year;
-   private boolean isLeapYear;
-
-   public LeapYear() {
-   }
-
    // write this
-   // Sets isLeapYear to true if year is divisible by 4, unless it is 
+   // return true if year is divisible by 4, unless it is 
    // also divisible by 100 and not divisible by 400. 
-   // Otherwise isLeapYear is set to false.
-   public void setLeapYear() {
+   // Otherwise return false
+   public boolean setLeapYear(int year) {
       
       
       
